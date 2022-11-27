fb743cd60f4a3138328b90ed5ec16bc995a980d8
gram655 <terranysj@gmail.com>
2022-11-27 22:54:33 +0900

    JQuery Ajax를 사용하여 Cascading 드롭다운리스트 구현하기

---
+++ VisualAcademy/Controllers/SelectListDemoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using VisualAcademy.Data;
+
+namespace VisualAcademy.Controllers
+{
+    public class SelectListDemoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SelectListDemoController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["PropertyId"] = new SelectList(_context.Properties.OrderBy(it => it.Name), "Id", "Name");
+            ViewData["LocationId"] = new SelectList(_context.Locations.OrderBy(it => it.Name), "Id", "Name");
+            ViewData["SublocationId"] = new SelectList(_context.Sublocations.OrderBy(it => it.SublocationName), "Id", "SublocationName");
+
+            return View();
+        }
+    }
+}
--- VisualAcademy/Program.cs
+++ VisualAcademy/Program.cs
@@ -39,5 +39,9 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
